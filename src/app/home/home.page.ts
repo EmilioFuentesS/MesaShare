4b9f192d6d252e6989d644c42adc33c2e7512811
Bryanchavezv <bryanchavezale@gmail.com>
2024-09-10 18:57:47 -0300

    Home Page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
 
-  constructor() {}
+  @ViewChild('animatedImage', { read: ElementRef, static: true }) animatedImage!: ElementRef;
 
+  constructor(private animationCtrl: AnimationController) {}
+
+  ngAfterViewInit() {
+    this.playAnimation();
+  }
+
+  playAnimation() {
+    const imageAnimation = this.animationCtrl.create()
+      .addElement(this.animatedImage.nativeElement)
+      .duration(3000) // Duración de la animación
+      .easing('ease-in-out') // Curva de aceleración/desaceleración suave
+      .fromTo('opacity', 0, 1) // Efecto de fade-in
+      .fromTo('transform', 'scale(0.8) rotate(0deg)', 'scale(1) rotate(360deg)'); // Efecto de escala + rotación completa
+  
+    imageAnimation.play();
+  }
+  
+  
 }
