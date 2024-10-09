4e4528353bc98a9af3477b85b2775cda5d8ffeb3
EmilioFuentesS <emilio.fuentes.cw@gmail.com>
2024-10-09 15:34:25 -0300

    admin y listar

--- src/app/admin/admin.page.ts
+++ src/app/admin/admin.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MesaAPIService, ClMenuItem } from '../MesaAPI/mesa-api.service'; // Asegúrate de usar la ruta correcta
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.page.scss'],
 })
 export class AdminPage implements OnInit {
+  productos: ClMenuItem[] = []; // Lista de productos
+  username: string | null = null;
 
-  constructor() { }
+  constructor(private router: Router, private mesaAPIService: MesaAPIService) {}
 
   ngOnInit() {
+    // Cargar los productos desde el servicio
+    this.cargarProductos();
   }
 
+  cargarProductos() {
+    this.mesaAPIService.getMenuItems().subscribe((data: ClMenuItem[]) => {
+      this.productos = data; // Asignar los productos a la variable
+    }, error => {
+      console.error('Error al cargar productos:', error);
+    });
+  }
+
+  onEliminarProducto(productId: number) {
+    // Eliminar directamente sin confirmación
+    this.mesaAPIService.deleteMenuItem(productId).subscribe({
+      next: () => {
+        console.log(`Producto ${productId} eliminado`);
+        // Volver a cargar la lista de productos después de eliminar
+        this.cargarProductos();
+      },
+      error: (err) => {
+        console.error("Error al eliminar producto", err);
+      }
+    });
+  }
 }
